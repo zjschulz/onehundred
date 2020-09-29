f9397e5b707e2314e7cd192b991a5c9dcf470790
zjschulz <zjschulz@gmail.com>
2020-09-29 14:17:22 +0000

    create fetch request to api for handleclick event

--- onehundred-frontend/src/Two.js
+++ onehundred-frontend/src/Two.js
@@ -6,30 +6,39 @@ export default class Two extends Component {
         super(props);
 
         this.state = {
-            quotes: []
+            quote: "",
+            author: ""
         }
 
         this.handleClick = this.handleClick.bind(this);
     }
 
-    componentDidMount() {
-        //fetch from quote web page or backend to populate state
-        //https://blog.hubspot.com/sales/famous-quotes
-        fetch('https://blog.hubspot.com/sales/famous-quotes')
-        .then(response => response.text())
-        .then(data => console.log(data))
-        .catch(err => alert(err))
-      }
-
     handleClick(event) {
         event.preventDefault();
+        fetch('http://quotes.rest/qod.json')
+        .then(response => response.json())
+        .then(data => this.setState({
+            quote: data.contents.quotes[0].quote,
+            author: data.contents.quotes[0].author
+        }))
+        .catch(err => alert(err))
         //populate a div element with a new quote
     }
 
     render () {
         return (
-        <div className="One">
-            <button onClick={this.handleClick}>Generate New Quote</button>
+        <div className="Two">
+            <button onClick={this.handleClick}>Generate Quote of the Day</button>
+            <span style={{zIndex:'50',fontSize:'0.9em', fontWeight: 'bold'}}>
+                <img src="https://theysaidso.com/branding/theysaidso.png" height="20" width="20" alt="theysaidso.com"/>
+                    <a href="https://theysaidso.com" title="Powered by quotes from theysaidso.com" style={{color: '#ccc', marginLeft: '4px', verticalAlign: 'middle'}}>
+                    They Said So®
+                </a>
+            </span>
+            <div className="qod" style={{background: 'white'}}>
+                <h1>{this.state.quote}</h1>
+                <h2>{this.state.author}</h2>
+            </div>
         </div>
         )
     }
