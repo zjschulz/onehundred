ffcc0e591ffbb533c8d387ceaed9f587ca399412
zjschulz <zjschulz@gmail.com>
2020-10-02 18:11:20 +0000

    create filter attribute and functions to change filter. create showItems function that depends on foliter

--- onehundred-frontend/src/components/Six.js
+++ onehundred-frontend/src/components/Six.js
@@ -14,26 +14,57 @@ export default class Six extends Component {
                 {name: "Wedding Cake", type: "Cake", price: 25},
                 {name: "Cupcakes", type: "Cake", price: 8},
                 {name: "Glazed", tpye: "Doughnut", price: 7}
-            ]
+            ],
+            filter: "All"
         }
 
         this.showItems = this.showItems.bind(this);
+        this.showAll = this.showAll.bind(this);
+        this.showDoughnuts = this.showDoughnuts.bind(this);
+        this.showCakes = this.showCakes.bind(this);
 
     }
 
     showItems() {
-        return this.state.items.map((item, index) => <SixA
-        key = {index}
-        name = {item.name}
-        price = {item.price}
-        />)
+        if (this.state.filter === "All") {
+            return this.state.items.map((item, index) => <SixA
+            key = {index}
+            name = {item.name}
+            price = {item.price}
+            />)
+        }
+        else {
+            return this.state.items.filter(item => item.type === this.state.filter).map((item, index) => <SixA
+            key = {index}
+            name = {item.name}
+            price = {item.price}
+            />)
+        }
+    }
+
+    showAll() {
+        this.setState({
+            filter: "All"
+        })
+    }
+
+    showDoughnuts() {
+        this.setState({
+            filter: "Doughnut"
+        })
+    }
+
+    showCakes() {
+        this.setState({
+            filter: "Cake"
+        })
     }
 
     render () {
         return (
         <div className="Six" style={{background: 'lightgrey', borderStyle: 'solid', margin: '1rem', textAlign: 'center'}}>
-            <button>All</button><button>Doughnut</button><button>Cake</button>
-            <div id="itemcards" style={{display: 'flex', flexWrap: "wrap"}}>{this.showItems()}</div>
+            <button onClick={this.showAll}>All</button><button onClick={this.showDoughnuts}>Doughnut</button><button onClick={this.showCakes}>Cake</button>
+        <div id="itemcards" style={{display: 'flex', flexWrap: "wrap"}}>{this.showItems()}</div>
         </div>
         )
     }
