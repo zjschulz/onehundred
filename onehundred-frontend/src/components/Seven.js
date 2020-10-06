7ff50a4910e97581667f88ddb5f5bb20feaa6f6c
zjschulz <zjschulz@gmail.com>
2020-10-06 15:07:02 +0000

    create and apply update display for button inputs

--- onehundred-frontend/src/components/Seven.js
+++ onehundred-frontend/src/components/Seven.js
@@ -12,7 +12,7 @@ export default class Seven extends Component {
 
         this.evaluateDisplay = this.evaluateDisplay.bind(this);
         this.clearDisplay = this.clearDisplay.bind(this);
-
+        this.updateDisplay = this.updateDisplay.bind(this);
     }
 
     evaluateDisplay () {
@@ -27,6 +27,12 @@ export default class Seven extends Component {
         })
     }
 
+    updateDisplay(event) {
+        this.setState({
+            display: this.state.display += event.target.value
+        })
+    }
+
     render () {
         return (
         <div className="Seven" style={{background: 'lightgrey', borderStyle: 'solid', margin: '1rem', textAlign: 'center'}}>
@@ -34,21 +40,21 @@ export default class Seven extends Component {
         <div className="screen">{this.state.display}</div>
             </form>
             <div className="buttons">
-                <button type="button" className="btn btn-yellow" data-num="*">*</button>
-                <button type="button" className="btn btn-yellow" data-num="/">/</button>
-                <button type="button" className="btn btn-yellow" data-num="-">-</button>
-                <button type="button" className="btn btn-yellow" data-num="+">+</button>
-                <button type="button" className="btn btn-grey" data-num=".">.</button>
-                <button type="button" className="btn btn-grey" data-num="9">9</button>
-                <button type="button" className="btn btn-grey" data-num="8">8</button>
-                <button type="button" className="btn btn-grey" data-num="7">7</button>
-                <button type="button" className="btn btn-grey" data-num="6">6</button>
-                <button type="button" className="btn btn-grey" data-num="5">5</button>
-                <button type="button" className="btn btn-grey" data-num="4">4</button>
-                <button type="button" className="btn btn-grey" data-num="3">3</button>
-                <button type="button" className="btn btn-grey" data-num="2">2</button>
-                <button type="button" className="btn btn-grey" data-num="1">1</button>
-                <button type="button" className="btn btn-grey" data-num="0">0</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-yellow" value="*">*</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-yellow" value="/">/</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-yellow" value="-">-</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-yellow" value="+">+</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value=".">.</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="9">9</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="8">8</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="7">7</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="6">6</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="5">5</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="4">4</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="3">3</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="2">2</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="1">1</button>
+                <button onClick={this.updateDisplay} type="button" className="btn btn-grey" value="0">0</button>
                 <button onClick={this.evaluateDisplay} type="button" className="btn-equal btn-grey">=</button>
                 <button onClick={this.clearDisplay} type="button" className="btn-clear btn-grey">C</button>
             </div>
