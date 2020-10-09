bf47a7f7b55ab441ac808741da61b2a56bc691d6
zjschulz <zjschulz@gmail.com>
2020-10-09 14:36:46 +0000

    restyle justification of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,18 @@ export default class Navbar extends React.Component {
       <div>
         <h1>100 Components of Code</h1>
         <nav className="Navbar">
+          <div>
             <a href="#One">One</a>
             <a href="#Two">Two</a>
             <a href="#Three">Three</a>
             <a href="#Four">Four</a>
+          </div>
+          <div>
             <a href="#Five">Five</a>
             <a href="#Six">Six</a>
             <a href="#Seven">Seven</a>
             <a href="#Eight">Eight</a>
+          </div>
         </nav>
       </div>
     )
